dd6977d205b98367f41d1670e9802da249c68dad
Nathan Moore <ArmchairArmada@Gmail.com>
2013-10-13 23:23:57 -0400

    AnimSprite
    Animated sprite

--- src/gameobjects/testobject.py
+++ src/gameobjects/testobject.py
@@ -3,14 +3,16 @@
 from gameobject import GameObject
 import pygame
 import components
+import assets
 
 class TestObject(GameObject):
     def __init__(self, scene, name, x, y, **kwargs):
         super(TestObject, self).__init__(scene, name, x, y, **kwargs)
 
         # TODO: All of this should be put into a component, obviously
-        self.sprite = components.StaticSprite(self, "testing/test.png")
-        self.timeout = 2000
+        #self.sprite = components.StaticSprite(self, assets.getImage("testing/test.png"))
+        self.sprite = components.AnimSprite(self, assets.getSpriteAnim("testing/test_anim.json"))
+        self.timeout = 5000
 
     def init(self):
         self.scene.object_mgr.normal_update.append(self)
@@ -20,7 +22,8 @@ class TestObject(GameObject):
         self.scene.object_mgr.normal_update.remove(self)
 
     def update(self, td):
-        self.x += td * 0.01
+        self.sprite.updateAnim(td)
+        self.x += td * 0.02
         self.timeout -= td
         if self.timeout <= 0:
             self.kill()
