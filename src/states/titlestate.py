e1359f04b6c6183dc85da47dd9859530e2ee4e9e
ArmchairArmada <482946+ArmchairArmada@users.noreply.github.com>
2013-10-19 23:25:52 -0400

    UI, EMT now glows when active

--- src/states/titlestate.py
+++ src/states/titlestate.py
@@ -6,15 +6,27 @@ The Pause state pauses the game and displays a darkened view of the old state
 
 from states import State
 import statemgr
-import inputs
 import assets
 import statevars
+import ui
 
 class TitleState(State):
     """Base class for all game states to derive from"""
     def __init__(self):
         super(TitleState, self).__init__()
         self.image = assets.getImage("graphics/title.png")
+        self.ui = ui.UI(220, 170)
+        self.ui.add(ui.Button(0, 0, "Continue", self.btnContinue))
+        self.ui.add(ui.Button(0, 20, "New Game", self.btnNewGame))
+
+    def btnContinue(self):
+        statevars.load("saves/save_1.json")
+        statemgr.switch("play")
+
+    def btnNewGame(self):
+        statevars.variables = {}
+        statevars.save("saves/save_1.json")
+        statemgr.switch("play")
 
     def gainFocus(self, previous, previous_name, *args, **kwargs):
         """What should be done when the state gets focus.  Previous is the state that had focus before this one."""
@@ -25,12 +37,11 @@ class TitleState(State):
         pass
 
     def update(self, td):
-        if inputs.getPausePress():
-            statevars.load("saves/save_1.json")
-            statemgr.switch("play")
+        self.ui.update(td)
 
     def draw(self, surface):
         surface.blit(self.image, (0,0))
+        self.ui.draw(surface)
 
     def debug_draw(self, surface):
         pass
